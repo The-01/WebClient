becefa8f7eeb990198969e357c0e9b7e0cc62c50
Atif <atiff091@gmail.com>
2022-09-06 22:29:17 +0500

    Add project files.

---
+++ WebClient/Models/Page.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClient.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Content { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
